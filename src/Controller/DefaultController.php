b11f7ccc5e4f96bb4c4ba5281c2be205c968ffba
pjehan <pierre.jehan@gmail.com>
2018-01-22 10:47:26 +0100

    encore

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,43 +11,10 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/default/{id}", name="default")
+     * @Route("/", name="default")
      */
-    public function index(Author $author)
+    public function index()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        //$books = $em->getRepository(Book::class)->findBy(['author' => $author]);
-
-        echo $author->getFullname();
-
-        foreach ($author->getBooks() as $book) {
-            echo $book->getTitle();
-        }
-        die;
-
-        /*
-        $pierre = $em->getRepository(Author::class)->find(4);
-
-        $book = new Book();
-        $book->setTitle("Batbook");
-        $book->setImage("batman.jpg");
-        $book->setSlug("batman-book");
-        $book->setCreatedAt(new \DateTime());
-        $book->setAuthor($pierre);
-
-        $em->persist($book);
-        $em->flush();
-        */
-
-        /*
-        $results = $em->getRepository(Author::class)->searchLike('Pi');
-        foreach ($results as $result) {
-            echo $result->getFullname();
-        }
-        die;
-        */
-
-        return new Response('Welcome to your new controller!');
+        return $this->render('default/index.html.twig');
     }
 }
